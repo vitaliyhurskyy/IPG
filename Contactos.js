d57a344dade2ee9260b1c3445f3042b4889e69fb
Vitaliy Hurskyy <vitaliyhurskyy@outlook.com>
2021-01-06 14:53:28 +0000

    Atualização de Contactos.html

--- Contactos.js
+++ Contactos.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "sender@email_address.com",
+        Password: "Enter your password",
+        To: 'vitaliyhurskyy@gmail.com',
+        From: "vitaliyhurskyy@gmail.com",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
+    })
+        .then(function (message) {
+            alert("mail sent successfully")
+        });
+} 
